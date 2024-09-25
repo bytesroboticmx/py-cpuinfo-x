50e78a0577d0488e4481b41f884e6ee6a8f4eda9
bytesroboticmx <120799721+bytesroboticmx@users.noreply.github.com>
2024-09-25 15:03:45 -0600

    Update py-cpuinfo.py

--- py-cpuinfo.py
+++ py-cpuinfo.py
@@ -1,17 +1,23 @@
+#Programa que utiliza la librería cpuinfo para mostrar información de la CPU
+#Autor:
+#Matrícula:
+#Version:
+#Fecha:
 import cpuinfo
+
 def mostrar_informacion_cpu():
     info_cpu = cpuinfo.get_cpu_info()
     print("Información de la CPU:")
-    print(f"Nombre del procesador: {info_cpu['brand_raw']}")
-    print(f"Fabricante:{info_cpu['vendor_id_raw', 'Informacion no disponible']}")
-    print(f"Modelo: {info_cpu['brand']}")
-    print(f"Arquitectura: {info_cpu["arch"]}")
-    print(f"Núcleos físicos: {info_cpu['count']}")
-    print(f"Frecuencia: {info_cpu['hz_advertised_friendly', 'Informacion no disponible']}")
-    print(f"Cache L1: {info_cpu['l1_data_cache_size', 'Informacion no disponible']}")
-    print(f"Soporte de 64 bits: {info_cpu['bits']}")
-    print(f"Soporte de virtualización: {info_cpu['vmx']}")
-    print(f"Soporte de Hyper-Threading: {info_cpu['ht']}")
+    print(f"Nombre del procesador: {info_cpu.get('brand_raw', 'Información no disponible')}")
+    print(f"Fabricante: {info_cpu.get('vendor_id_raw', 'Información no disponible')}")
+    print(f"Modelo: {info_cpu.get('brand_raw', 'Información no disponible')}")
+    print(f"Arquitectura: {info_cpu.get('arch', 'Información no disponible')}")
+    print(f"Núcleos físicos: {info_cpu.get('count', 'Información no disponible')}")
+    print(f"Frecuencia: {info_cpu.get('hz_advertised_friendly', 'Información no disponible')}")
+    print(f"Cache L1: {info_cpu.get('l1_data_cache_size', 'Información no disponible')}")
+    print(f"Soporte de 64 bits: {info_cpu.get('bits', 'Información no disponible')}")
+    print(f"Soporte de virtualización: {info_cpu.get('vmx', 'Información no disponible')}")
+    print(f"Soporte de Hyper-Threading: {info_cpu.get('ht', 'Información no disponible')}")
 
 if __name__ == "__main__":
     mostrar_informacion_cpu()
